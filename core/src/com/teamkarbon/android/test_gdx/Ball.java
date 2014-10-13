f8c4eb444ccae1ba84a117cc73a5bd7ab8a2b126
euwbah <euwbah@gmail.com>
2014-10-13 17:46:05 +0800

    Started work on GDX.. learning stuff now

---
+++ core/src/com/teamkarbon/android/test_gdx/Ball.java
@@ -0,0 +1,17 @@
+package com.teamkarbon.android.test_gdx;
+
+public class Ball {
+    int x;
+    int y;
+
+    public Ball(int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+
+    public void setPos(int xpos, int ypos)
+    {
+        x = xpos;
+        y = ypos;
+    }
+}
--- core/src/com/teamkarbon/android/test_gdx/derptest.java
+++ core/src/com/teamkarbon/android/test_gdx/derptest.java
@@ -2,26 +2,32 @@ package com.teamkarbon.android.test_gdx;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class derptest extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    ShapeRenderer shapeRenderer;
+    Camera camera;
+    World world;
+    Body body;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        shapeRenderer.setProjectionMatrix(camera.combined);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(0, 1, 1, 1);
+        shapeRenderer.circle(40, 50, 30);
+        shapeRenderer.end();
 	}
+
+
 }
