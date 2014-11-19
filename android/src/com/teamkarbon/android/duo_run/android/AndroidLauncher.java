6cc0e41e80d4e1fdf7615543f3b88b6c8a32f634
darkpolice <joe@teamkarbon.com>
2014-11-19 22:29:36 +0800

    Added proguard-project.txt so stuff wouldn't mess up

--- android/src/com/teamkarbon/android/duo_run/android/AndroidLauncher.java
+++ android/src/com/teamkarbon/android/duo_run/android/AndroidLauncher.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.teamkarbon.android.duo_run.derptest;
 
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
