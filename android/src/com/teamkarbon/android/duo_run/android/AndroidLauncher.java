aae0fa2d75b8fe3016d5cde7072a97bde78328d5
darkpolice <joe@teamkarbon.com>
2014-11-24 21:16:08 +0800

    Added more google services code!

--- android/src/com/teamkarbon/android/duo_run/android/AndroidLauncher.java
+++ android/src/com/teamkarbon/android/duo_run/android/AndroidLauncher.java
@@ -1,22 +1,39 @@
 package com.teamkarbon.android.duo_run.android;
 
+import com.teamkarbon.android.duo_run.derptest;
+
+import android.content.Context;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.teamkarbon.android.duo_run.derptest;
-import com.google.android.gms.common.*;
-import com.google.android.gms.drive.*;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.Player;
+import com.google.android.gms.plus.Plus;
 
 
 
 public class AndroidLauncher extends AndroidApplication implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private static final int RC_SIGN_IN = 0;
-
+    // Client used to interact with Google APIs
     private GoogleApiClient mGoogleApiClient;
+
+    // Are we currently resolving a connection failure?
+    private boolean mResolvingConnectionFailure = false;
+
+    // Has the user clicked the sign-in button?
+    private boolean mSignInClicked = false;
+
+    // Automatically start the sign-in flow when the Activity starts
+    private boolean mAutoStartSignInFlow = true;
+
+    // request codes we use when invoking an external activity
+    private static final int RC_RESOLVE = 5000;
+    private static final int RC_UNUSED = 5001;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,45 +42,80 @@ public class AndroidLauncher extends AndroidApplication implements GoogleApiClie
         initialize(new derptest(), config);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Drive.API)
-                .addScope(Drive.SCOPE_FILE)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
                 .build();
     }
 
     @Override
-    public void onConnected(Bundle connectionHint) {
-        // Connected to Google Play services!
-        // The good stuff goes here.
+    public void onConnected(Bundle bundle) {
+
     }
 
     @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection has been interrupted.
-        // Disable any UI components that depend on Google APIs
-        // until onConnected() is called.
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // This callback is important for handling errors that
-        // may occur while attempting to connect with Google.
-        // More about this in the next section.
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+            //if (!BaseGameUtils.resolveConnectionFailure(this, mGoogleApiClient, connectionResult,
+            //        RC_SIGN_IN, getString(R.string.signin_other_error))) {
+            //    mResolvingConnectionFailure = false;
+            //}
+        }
+
+        //TODO: When Sign in fails, do something
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //mGoogleApiClient.connect();
+    //TODO: Make this actually link to a button
+    public void onSignInButtonClicked() {
+        mSignInClicked = true;
+        mGoogleApiClient.connect();
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
+    //TODO: Make this actually link to a button
+    public void onSignOutButtonClicked() {
+        mSignInClicked = false;
+        Games.signOut(mGoogleApiClient);
         if (mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
+    }
 
+    //TODO: You can make this work if you want
+    //This basically records the user's score if he/she's offline, and push it to the cloud when the user comes online again
+    class AccomplishmentsOutbox {
+        boolean mPrimeAchievement = false;
+        boolean mHumbleAchievement = false;
+        boolean mLeetAchievement = false;
+        boolean mArrogantAchievement = false;
+        int mBoredSteps = 0;
+        int mEasyModeScore = -1;
+        int mHardModeScore = -1;
+
+        boolean isEmpty() {
+            return !mPrimeAchievement && !mHumbleAchievement && !mLeetAchievement &&
+                    !mArrogantAchievement && mBoredSteps == 0 && mEasyModeScore < 0 &&
+                    mHardModeScore < 0;
+        }
+
+        public void saveLocal(Context ctx) {
+            /* TODO: This is left as an exercise. To make it more difficult to cheat,
+             * this data should be stored in an encrypted file! And remember not to
+             * expose your encryption key (obfuscate it by building it from bits and
+             * pieces and/or XORing with another string, for instance). */
+        }
+
+        public void loadLocal(Context ctx) {
+            /* TODO: This is left as an exercise. Write code here that loads data
+             * from the file you wrote in saveLocal(). */
+        }
     }
 }
